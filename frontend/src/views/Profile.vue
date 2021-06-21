71b4b97b52d18912f4d75b702880c2aaad08f4ec
Sidharth Jaiswal <sidhankit@gmail.com>
2021-06-21 14:20:28 +0200

    modified profile view

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -13,9 +13,29 @@
       <strong>Id:</strong>
       {{currentUser.id}}
     </p>
+    <p>
+      <strong>Email:</strong>
+      {{currentUser.email}}
+    </p>
     <strong>Authorities:</strong>
     <ul>
       <li v-for="(role,index) in currentUser.roles" :key="index">{{role}}</li>
     </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Profile',
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    }
+  },
+  mounted() {
+    if (!this.currentUser) {
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
