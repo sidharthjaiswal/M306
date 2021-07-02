1b114c374e672f1b66d9c6c0c3efad5f4e9a3da7
Sidharth Jaiswal <55667630+sidharthjaiswal@users.noreply.github.com>
2021-07-02 09:10:51 +0200

    Revert "Added Rolf Dürr as our amazing Supporter"
    This reverts commit 265d4057eb525d2e276b5f4566ede0bc93232b46.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -14,76 +14,34 @@ Descripton:  Student Record Management system. It efficiently allows users to pe
 <template>
   <div class="container">
     <header class="jumbotron">
-      <!--  <h3>{{content}}</h3>-->
-      <h3>Hallo und willkommen</h3>
+    <!--  <h3>{{content}}</h3>-->
+    <h3>Hallo und willkommen</h3>
     </header>
-    <div class="kontakt">
-      Falls hilf bruchsch, kontaktier ihn.
-      <br />
-      <br />
-      <h4>Adresse:</h4>
-      Dürr Informatik GmbH<br />
-      Alte Winterthurerstrasse 218<br />
-      8309 Breite-Nürensdorf<br />
-      <br>
-      <table>
-        <tbody>
-          <tr>
-            <td>Telefon +41 (0)44 865 56 24</td>
-          </tr>
-          <tr>
-            <td> Telefax +41 (0)44 865 55 84</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <br>
-      Rofl Furry <br />
-    Elektroingenieur FH, Betriebsoekonom dipl. oek.<br>
-    E-Mail: rolf.duerr@duerr-informatik.ch
-    <div class="col"><img src="./Dürr.jpg" /></div>
   </div>
 </template>
 
-<style scoped>
-.col {
-  
-  flex: 1; /* additionally, equal width */
-  padding: 1em;
-  hyphens: auto;
-  text-align: justify;
-}
+<script>
+import UserService from '../services/user.service';
 
-.kontakt {
-  display: block;
-  font-family: Verdana, sans-serif;
-  font-size: 14px;
-}
-table {
-  display: block;
-  border-collapse: separate;
-  box-sizing: border-box;
-  text-indent: initial;
-  border-spacing: 2px;
-  border-color: grey;
-}
-
-tbody {
-  display: table-row-group;
-  vertical-align: middle;
-  border-color: inherit;
-}
-tr {
-  display: table-row;
-  vertical-align: inherit;
-  border-color: inherit;
-}
-td {
-  display: table-cell;
-  vertical-align: inherit;
-}
-#textfirma1 {
-    float: left;
-    padding-left: 10px;
-}
-</style>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      content: ''
+    };
+  },
+  mounted() {
+    UserService.getPublicContent().then(
+      response => {
+        this.content = response.data;
+      },
+      error => {
+        this.content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  }
+};
+</script>
